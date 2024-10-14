2f3c63a359d6c2c665de18d4e2a2ac166fe84cd0
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-10-14 14:35:31 +0300

    add screnshot ability

--- src/test/java/org/etiya/AppTest.java
+++ src/test/java/org/etiya/AppTest.java
@@ -1,15 +1,25 @@
 package org.etiya;
 
+import org.apache.commons.io.FileUtils;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
@@ -17,17 +27,60 @@ public class AppTest {
   FirefoxDriver firefoxDriver;
   WebDriverWait wait;
   SoftAssertions softAssertions;
+  String testName;
+
+  @BeforeEach // Her test öncesi çalıştırılacak ortak func.
+  public void setup() {
+    firefoxDriver = new FirefoxDriver();
+    wait = new WebDriverWait(firefoxDriver, Duration.ofSeconds(10));
+    softAssertions = new SoftAssertions();
+  }
+
+  @AfterEach
+  public void cleanup(){
+    takeScreenshotAfterTest();
+    firefoxDriver.quit();
+  }
+
+  private void takeScreenshotAfterTest() {
+    // Ekran görüntüsü al
+    File screenshot = firefoxDriver.getScreenshotAs(OutputType.FILE);
+
+    // Mevcut tarih formatı Gün-Ay-Yıl
+    String currentDate = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+
+    // Klasör yolunu oluştur
+    String folderPath = "C:\\Users\\klyyc\\Desktop\\Projects\\Selenium\\etiya.nine\\test-ss\\" + currentDate;
+
+    // Test adıyla birlikte dosya yolu
+    String filePath = folderPath + "\\" + testName + ".png";
+
+    try {
+      // Klasör yoksa oluştur
+      File directory = new File(folderPath);
+      if (!directory.exists()) {
+        directory.mkdirs(); // Klasör ve alt klasörleri oluştur
+      }
+
+      // Ekran görüntüsünü kaydet
+      FileUtils.copyFile(screenshot, new File(filePath));
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("SS alınamadı.");
+    }
+  }
   @Test
   public void testTitleShouldBeCorrect()
   {
+    testName = "testTitleShouldBeCorrect";
     String expectedResult = "Swag Labs";
     firefoxDriver.get("https://www.saucedemo.com/");
     String actualResult = firefoxDriver.getTitle();
     assertEquals(expectedResult, actualResult, "Titlelar uyuşmuyor.");
   }
-
   @Test
   public void testLoginWithCorrectCredentials() throws InterruptedException {
+    testName = "testLoginWithCorrectCredentials";
     firefoxDriver.get("https://www.saucedemo.com/");
     try{
       WebElement usernameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")));
@@ -49,22 +102,11 @@ public class AppTest {
 
       softAssertions.assertThat(firefoxDriver.getCurrentUrl())
               .as("Giriş yapıldıktan sonra yönlendirilen url yanlış.")
-              .isEqualTo("https://www.saucedemo.com/inventory.html1");
-    }finally {
+              .isEqualTo("https://www.saucedemo.com/inventory.html");
+    }
+    finally {
       softAssertions.assertAll();
     }
-  }
-
-  @BeforeEach // Her test öncesi çalıştırılacak ortak func.
-  public void setup() {
-    firefoxDriver = new FirefoxDriver();
-    wait = new WebDriverWait(firefoxDriver, Duration.ofSeconds(10));
-    softAssertions = new SoftAssertions();
-  }
-
-  @AfterEach
-  public void cleanup(){
-    firefoxDriver.quit();
   }
 
 }
